66c25c2ce2ee3d42e1279b7766a7c054d92f1e3e
coleeeee-dev <eduardocossar@gmail.com>
2025-11-30 00:12:38 -0500

    m3

---
+++ src/main/java/com/sendify/platform/shared/infrastructure/configuration/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.sendify.platform.shared.infrastructure.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        // Acepta cualquier origen (Swagger, tu front, etc.)
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
